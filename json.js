956c2f92f552b18e6b64c236b2239cc2873ec887
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-07 12:03:48 -0400

    fixed formatting

--- json.js
+++ json.js
@@ -1,14 +1,13 @@
-const data = require("./data.json")
-const {tas, teachers} = data
+const data = require("./data.json");
+const { tas, teachers } = data;
 
-
-console.log("The Tas are:")
+console.log("The Tas are:");
 for (let i = 0; i < tas.length; i++) {
   let teachersAssistant = tas[i];
   console.log(teachersAssistant.name);
 }
 
-console.log("The Teachers are:")
+console.log("The Teachers are:");
 for (let i = 0; i < teachers.length; i++) {
-console.log(teachers[i].name);
+  console.log(teachers[i].name);
 }
